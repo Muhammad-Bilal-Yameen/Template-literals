0a73e2873cdbe718bb58aa99c345a897ec6608f6
Muhammad Bilal Yameen <bilalyameen03151190023@gmail.com>
2024-02-16 19:28:59 -0800

    Template Literals

---
+++ main.js
@@ -0,0 +1,44 @@
+// date 15-02-2024
+// Template Literals
+// Exercise 1
+// let num1 = 12
+// let num2 = 26
+// let result = num1 * num2
+// console.log(result);     // Ans is 312
+// Exercise 2
+var myName = "daniyal";
+var hello = "Hello" + " " + myName;
+console.log(hello); // hello daniyal
+// Exercise 3
+// let door = "brown"
+// let bike = "black"
+// let result = door +" "+ bike
+// console.log(result);        // Ans is brown black
+// Exercise 3
+// let computer = "me"
+// let mouse = "and"
+// let laptop = "you" +" "+ mouse +" "+ computer
+// console.log(laptop);                       // Ans is you and me
+// Exercise 4
+// let fName = "Mauhammad Bilal"
+// console.log(`My name is ${fName}`);   // Ans is Muhammad Bilal
+// Exercise 5
+// let hobbies = "study"
+// console.log(`my hobbies is ${hobbies}`);
+// console.log(`I am student 
+// I am muslim`);                            // Ans is my hobbies is study
+// I am student
+// I am muslim
+// Exercise 5
+// let user = "account"
+// console.log(`i am user of ${user}`);   // ans is i am user of acount
+// Exercise 6
+// let num:number = 786
+// console.log(`my favourite number is ${num}`)    // ans is my favourite number is 786 
+// Exercise 7
+// let masjid = "namaz"
+// let house = "roza"
+// console.log(`first letter is ${masjid}
+// second number is ${house}
+// `);                                   // ans is first number is namaz
+// second number is second number is roza
---
+++ main.ts
@@ -0,0 +1,75 @@
+// date 15-02-2024
+
+// Template Literals
+
+// Exercise 1
+// let num1 = 12
+// let num2 = 26
+// let result = num1 * num2
+
+// console.log(result);     // Ans is 312
+
+
+
+// Exercise 2
+
+// let myName = "daniyal";
+// let hello = "Hello"+" "+ myName
+// console.log(hello);          // Hello daniyal
+
+
+// Exercise 3
+
+// let door = "brown"
+// let bike = "black"
+// let result = door +" "+ bike
+
+// console.log(result);        // Ans is brown black
+
+
+
+// Exercise 4
+
+// let computer = "me"
+// let mouse = "and"
+// let laptop = "you" +" "+ mouse +" "+ computer
+// console.log(laptop);                       // Ans is you and me
+
+ 
+// Exercise 5
+
+let fName = "Mauhammad Bilal"
+console.log(`My name is ${fName}`);   // Ans is Muhammad Bilal
+
+// Exercise 6
+
+                            // Ans is my hobbies is study
+                                          // I am student
+                                          // I am muslim
+
+// Exercise 7
+
+// let user = "account"
+// console.log(`i am user of ${user}`);   // ans is i am user of acount
+
+// Exercise 8
+
+// let num:number = 786
+// console.log(`my favourite number is ${num}`)    // ans is my favourite number is 786 
+
+
+// Exercise 9
+
+// let masjid = "namaz"
+// let house = "roza"
+// console.log(`first letter is ${masjid}
+// second number is ${house}
+// `);                                   // ans is first number is namaz
+                                    // second number is second number is roza
+
+
+
+
+
+
+
